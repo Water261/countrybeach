22663b515694606d6147bb9b4828b6235d54a7a3
Matthew Wilks <water261@outlook.com.au>
2023-06-26 16:24:26 +1000

    Create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>Not Found - Country Beach</title>
+</svelte:head>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center text-white">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Not Found</h1>
+			<p class="py-6">We can't seem to find what you're looking for.</p>
+			<button class="btn btn-primary" on:click={() => window.history.back()}>Go Back?</button>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	div.hero {
+		background-image: url('$lib/img/login-img.svg');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+</style>
