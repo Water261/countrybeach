a3209b748f5338c6b7c0f1b9945d56f5223f9f63
Matthew Wilks <water261@outlook.com.au>
2023-06-26 15:25:29 +1000

    Rename client to prismaClient

--- src/lib/server/DatabaseClient.ts
+++ src/lib/server/DatabaseClient.ts
@@ -3,10 +3,10 @@ import { PrismaClient } from "@prisma/client";
 export class DatabaseClient {
 	static instance: DatabaseClient;
 
-	public client: PrismaClient;
+	public prismaClient: PrismaClient;
 
 	private constructor() {
-		this.client = new PrismaClient();
+		this.prismaClient = new PrismaClient();
 	}
 
 	static getInstance(): DatabaseClient {
