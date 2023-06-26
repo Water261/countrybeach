1eec2aa93c82b15448b603d507dbc750dfd2c1ea
Matthew Wilks <water261@outlook.com.au>
2023-06-26 15:12:42 +1000

    Add data seeding

---
+++ prisma/seed.js
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import data from "./user-seed.json" assert { type: "json" };
+
+const client = new PrismaClient();
+
+for (const user of data) {
+	await client.user.create({
+		data: user
+	})
+}
+
+client.$disconnect()
