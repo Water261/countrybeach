02fb791d24585fbf75333e6ea854daef671aa9bd
Matthew Wilks <water261@outlook.com.au>
2023-06-27 15:31:23 +1000

    Fix data loading issues

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
+	import type { PageData } from './$types';
 
-	export let pageData: PageData;
+	export let data: PageData;
 </script>
 
 <svelte:head>
 	<title>Dashboard - Country Beach</title>
 </svelte:head>
+
+<h1 class="text-5xl font-bold">{data.firstName} {data.lastName}</h1>
