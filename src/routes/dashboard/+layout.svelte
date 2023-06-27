46ede6055a04f88d6e72ea1b1d7fe4a0de269155
Matthew Wilks <water261@outlook.com.au>
2023-06-27 15:03:26 +1000

    Add side navbar

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,90 @@
+<div class="drawer lg:drawer-open">
+	<input type="checkbox" id="drawer" class="drawer-toggle" />
+	<div class="drawer-content">
+		<!-- TODO: Fix mobile nav -->
+		<label for="drawer" class="btn btn-ghost lg:hidden">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+				/>
+			</svg>
+		</label>
+		<slot />
+	</div>
+	<div class="drawer-side shadow p-4 bg-base-100">
+		<label for="drawer" class="drawer-overlay" />
+		<div class="flex flex-col items-center justify-between h-full">
+			<div>
+				<a href="/dashboard" class="text-2xl btn btn-ghost normal-case">
+					<div class="inline-flex">
+						<span>Country</span>
+						<span class="text-secondary">Beach</span>
+					</div>
+				</a>
+				<div class="divider"></div>
+				<div class="flex flex-col w-full">
+					<a href="/dashboard" class="btn btn-ghost w-full">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+							/>
+						</svg>
+						My Info
+					</a>
+					<a href="/dashboard/staff" class="btn btn-ghost w-full">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+							/>
+						</svg>
+						My Staff
+					</a>
+				</div>
+			</div>
+			<button class="btn btn-ghost text-lg w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+					/>
+				</svg>
+				Log Out
+			</button>
+		</div>
+	</div>
+</div>
