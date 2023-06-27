7c866f601889f910d918d9011daa9ee41be1413e
Matthew Wilks <water261@outlook.com.au>
2023-06-27 15:26:23 +1000

    Remove confusing description

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,8 +9,7 @@
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content text-center text-white">
 		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">{ $page.status } - { $page.error?.message }</h1>
-			<p class="py-6">We can't seem to find what you're looking for.</p>
+			<h1 class="text-5xl font-bold my-4">{ $page.status } - { $page.error?.message }</h1>
 			<button class="btn btn-primary" on:click={() => window.history.back()}>Go Back?</button>
 		</div>
 	</div>
