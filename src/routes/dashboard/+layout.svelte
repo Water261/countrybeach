26b73837c6fad6052dc6b698193fb60db496eaa2
Matthew Wilks <water261@outlook.com.au>
2023-07-11 18:04:40 +1000

    Make icons level

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -43,7 +43,7 @@
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							class="w-6 h-6"
+							class="w-6 h-6 relative top-1"
 						>
 							<path
 								stroke-linecap="round"
@@ -60,7 +60,7 @@
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							class="w-6 h-6"
+							class="w-6 h-6 relative top-1"
 						>
 							<path
 								stroke-linecap="round"
