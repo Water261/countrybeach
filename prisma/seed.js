43fc28ea02f61ed8e8663166d65fda55649f13cc
Matthew Wilks <water261@outlook.com.au>
2023-07-12 13:54:16 +1000

    Update seed script

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import data from './user-seed.json' assert { type: 'json' };
+import { hash } from 'bcrypt';
 
 const client = new PrismaClient();
+const defaultPassword = await hash("password", 10);
 
 for (const user of data) {
 	await client.user.create({
-		data: user
+		data: {...user, password: defaultPassword}
 	});
 }
 
