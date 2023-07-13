9790027c488c28fac20ce6c48985e4e1ca8828c7
Matthew Wilks <water261@outlook.com.au>
2023-07-13 16:17:42 +1000

    Add basic logging in login page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,14 +3,17 @@ import type { PageServerLoad } from './$types';
 import { isValidSession } from '$lib/server/isValidSession';
 
 export const load: PageServerLoad = async ({ cookies }) => {
+	console.log("Got new login page request");
 	const sessionId = cookies.get('SESSION_ID');
 
 	if (sessionId !== undefined) {
 		const validSession = await isValidSession(sessionId);
 
 		if (validSession) {
+			console.log("Valid session found, redirecting to dashboard");
 			throw redirect(303, '/dashboard');
 		} else {
+			console.log("Invalid session found, informing client to delete cookie");
 			cookies.delete('SESSION_ID');
 		}
 	}
