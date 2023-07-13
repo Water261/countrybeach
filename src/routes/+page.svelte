9908a0c8aac7452c2164df84dd161deb6c330456
Matthew Wilks <water261@outlook.com.au>
2023-07-13 16:09:57 +1000

    Increase text size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -95,7 +95,7 @@
 			</div>
 
 			<div class="form-control w-full">
-				<button type="submit" class="btn btn-secondary">Log In</button>
+				<button type="submit" class="btn btn-secondary text-xl">Log In</button>
 			</div>
 		</form>
 		<p>&copy; {new Date().getFullYear()} - Country Beach</p>
