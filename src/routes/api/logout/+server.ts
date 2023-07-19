f335750aaec31e563a3f9eb95369e8f62669efc2
Matthew Wilks <water261@outlook.com.au>
2023-07-19 19:18:52 +1000

    Update logout api

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,22 +1,16 @@
+import { DbClient } from '../../../hooks.server';
 import type { RequestHandler } from './$types';
-import { DatabaseClient } from '$lib/server/DatabaseClient';
-
-const DB_CLIENT = DatabaseClient.getInstance();
 
 export const POST: RequestHandler = async ({ cookies }) => {
 	console.log('Got new logout request');
-	const sessionId = cookies.get('SESSION_ID');
+	const sessionId = cookies.get('SESSION_ID') ?? '';
 
-	if (sessionId === undefined) {
-		console.log('Client provided an invalid session');
-		return new Response('401: Unauthorized', { status: 401, statusText: 'Unauthorized' });
-	}
-
-	await DB_CLIENT.prismaClient.sessions.delete({
-		where: {
-			id: sessionId
-		}
-	});
-
-	return new Response('200: Ok', { status: 200, statusText: 'Ok' });
+	return await DbClient.session
+		.delete({
+			where: {
+				sessionId
+			}
+		})
+		.catch(() => new Response(null, { status: 500, statusText: 'Internal Server Error' }))
+		.then(() => new Response(null, { status: 200, statusText: 'Ok' }));
 };
